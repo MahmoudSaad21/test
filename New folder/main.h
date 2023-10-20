9de2fa2e03a8f37bea9ae75725f46f3780d8599b
MahmoudSaad21 <86734676+MahmoudSaad21@users.noreply.github.com>
2023-10-21 02:43:05 +0300

    Update main.h

--- New folder/main.h
+++ New folder/main.h
@@ -1,6 +1,7 @@
-main.h
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stddef.h>
 
 struct Alias {
     char *name;
@@ -13,5 +14,7 @@ int areStringsEqual(const char *str1, const char *str2);
 void write_custom(int fd, const char *str);
 int compareEnvVars(const char *a, const char *b);
 void sortEnvVars(const char **envVars, int envCount);
+void handle_alias(struct Alias aliases[], int *alias_count);
+void handle_command(struct Alias aliases[], int alias_count, char* args[], int* last_command_exit_status);  /* Make sure this line is here */
 
 #endif
